d40ead4bcef80493ed9d7f60236a67354f076126
Théo Matussière <theo@matussie.re>
2022-01-04 11:04:01 +0100

    secondspace

--- days/day3bis.ts
+++ days/day3bis.ts
@@ -2,15 +2,14 @@ import type Ip5 from "p5";
 import { frame, unif, unifctr } from "./rnd";
 
 const serverSideProps = {
-  name: "space",
-  path: "d3-space",
-  comment:
-    "moving! particles! fun to do, but not great to look at... probly won't run on mobile?",
+  name: "space2",
+  path: "space2",
+  comment: "was not satisfied with v1, now not satisfied with v2 yay",
   sketch: null,
 };
 
 const d2o = () => {
-  const n = 1_000;
+  const n = 2_000;
   const tau = 0.02;
 
   return {
@@ -26,32 +25,44 @@ const d2o = () => {
         s.setup = () => {
           const canvas = s.createCanvas(w, h);
           s.pixelDensity(1);
+          s.noSmooth();
         };
 
         const particles = [...Array(n)].map(() => ({
           speed: unif(0.7, 1.3),
           phase: unif(0, 2 * Math.PI),
-          radius: unif(1, 4),
-          vel: unifctr(w / 3, (2 * w) / 3),
-          hel: unifctr(h / 6, (5 * h) / 6),
+          radius: unif(1, 8),
+          vel: unifctr(w, (2 * w) / 3),
+          hel: unifctr(h / 4, h / 6),
           color: Math.floor(unif(100, 230)),
         }));
 
         s.draw = () => {
-          s.background("black");
+          // s.background("black");
           s.push();
+          s.fill(0, 90);
+          s.noStroke();
+          s.rect(0, 0, w, h);
+          s.pop();
+
+          s.push();
+          s.fill(60);
+          s.circle(w / 4, h / 2, w / 4);
+
           s.fill(240 + 10 * s.sin(3 * t));
-          s.circle(w / 2, h / 2, star * (1 + s.sin(t) / 8));
+          s.circle((-3 * w) / 4, h / 4, 2 * w);
           s.pop();
 
           particles.forEach((p) => {
-            const x = w / 2 + p.vel * s.cos(p.phase + p.speed * t);
+            const x = p.vel * s.cos(p.phase + p.speed * t);
             const y = h / 2 + p.hel * s.sin(p.phase + p.speed * t);
             const r = p.radius * (1.3 + s.cos(p.speed * t) / 4);
 
-            s.noStroke();
-            s.circle(x, y, r);
-            s.fill(p.color + 20 * s.cos(t));
+            if (x > w / 4 || y > h / 2) {
+              s.noStroke();
+              s.rect(x, y, r, r);
+              s.fill(p.color + 20 * s.cos(t));
+            }
           });
 
           t += tau;
--- days/index.ts
+++ days/index.ts
@@ -1,5 +1,6 @@
 import day2 from "./day2";
 import day3 from "./day3";
+import day3bis from "./day3bis";
 import day1 from "./day1";
 
 const days: Array<{
@@ -7,6 +8,6 @@ const days: Array<{
   name: string;
   comment: string;
   sketch: null | ((ref: HTMLDivElement) => void);
-}> = [day1, day2, day3];
+}> = [day1, day2, day3, day3bis];
 
 export default days;
